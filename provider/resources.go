82dd77a2d75a547e4a0514d2fd3c84896719519e
Garanzha Dmytro <dmytro.garanzha@howly.com>
2024-06-17 14:05:07 +0300

    provider

--- provider/resources.go
+++ provider/resources.go
@@ -134,7 +134,7 @@ func Provider() tfbridge.ProviderInfo {
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g https://github.com/org/pulumi-provider-name/releases/
-		PluginDownloadURL: "",
+		PluginDownloadURL: "github://api.github.com/howly-global",
 		Description:       "A Pulumi package for creating and managing airbyte cloud resources.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
 		// For all available categories, see `Keywords` in
